edce20892df115640e504c3af294af773d532314
Kookiie88 <alexander@jj-media.se>
2024-09-30 08:34:36 +0700

    Implement FridayDiscount class with discount logic and chaining in getDescription

---
+++ src/main/java/se/lernia/lab/FridayDiscount.java
@@ -0,0 +1,26 @@
+package se.lernia.lab;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class FridayDiscount extends BaseDiscount {
+
+  public FridayDiscount(Discount nextDiscount) {
+    super(nextDiscount);
+  }
+
+  @Override
+  protected boolean isApplicable(Product product) {
+    return LocalDate.now().getDayOfWeek() == DayOfWeek.FRIDAY;
+  }
+
+  @Override
+  protected double calculateDiscount(Product product) {
+    return product.price() * 0.10;
+  }
+
+  @Override
+  protected String getDiscountDescription() {
+    return "It's Friday! You get a 10% discount.";
+  }
+}
